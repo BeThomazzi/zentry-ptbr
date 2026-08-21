f597d80076d943e8cdb9a1442a9310cd767c04f7
Bernardo Thomazzi <bernardothomazzi@hotmail.com>
2026-08-20 22:31:47 -0300

    fix: fix links and texts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,18 +3,21 @@ import { IoLogoWhatsapp } from "react-icons/io";
 import { AiFillInstagram } from "react-icons/ai";
 
 const socialLinks = [
-  { href: "https://bethomazzi.github.io/portfolio/", icon: <FaGlobe /> },
+  { href: "https://devoxlabs.dev", icon: <FaGlobe /> },
   { href: "https://github.com/bethomazzi", icon: <FaGithub /> },
-  { href: "https://www.instagram.com/bethomazzi/", icon: <AiFillInstagram /> },
-  { href: "https://api.whatsapp.com/send?phone=5584996677053", icon: <IoLogoWhatsapp /> },
+  { href: "https://www.instagram.com/devox.labs/", icon: <AiFillInstagram /> },
+  { href: "https://api.whatsapp.com/send?phone=5584999675753", icon: <IoLogoWhatsapp /> },
 ];
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="w-screen bg-[#5542ff] py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm font-general font-light md:text-left ">
-          <a className="hover:underline  cursor-pointer" href="https://bethomazzi.github.io/portfolio/" target="_blank">©Bernardo Thomazzi</a> 2025. Todos os direitos reservados
+          ©
+          <a className="hover:underline  cursor-pointer" href="https://devoxlabs.dev" target="_blank"> Bernardo Thomazzi </a>
+          {currentYear}. Todos os direitos reservados
         </p>
 
         <div className="flex justify-center gap-4  md:justify-start">
